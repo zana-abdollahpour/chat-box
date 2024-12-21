59624d4e3630019e5c4cffd350fed4bed56b01ac
zana-abdollahpour <zana.ap4@gmail.com>
2024-12-21 13:38:35 +0330

    add useMutationState hook

---
+++ src/hooks/useMutationState.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useMutation } from "convex/react";
+import { useState } from "react";
+
+export const useMutaionState = (mutationToRun: any) => {
+  const [pending, setPending] = useState(false);
+  const mutationFn = useMutation(mutationToRun);
+
+  const mutate = (payload: any) => {
+    setPending(true);
+
+    return mutationFn(payload)
+      .then((res) => res)
+      .catch((err) => {
+        throw err;
+      })
+      .finally(() => setPending(false));
+  };
+
+  return { pending, mutate };
+};
