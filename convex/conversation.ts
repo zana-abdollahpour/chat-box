f3d23e874fee59e127a51f2a5510ad7afe4cb82c
zana-abdollahpour <zana.ap4@gmail.com>
2024-12-25 02:41:44 +0330

    implement backend for read receipts

--- convex/conversation.ts
+++ convex/conversation.ts
@@ -227,3 +227,45 @@ export const leaveGroup = mutation({
     await ctx.db.delete(membership._id);
   },
 });
+
+export const markRead = mutation({
+  args: {
+    conversationId: v.id("conversations"),
+    messageId: v.id("messages"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    const currentUser = await getUserByClerkId({
+      ctx,
+      clerkId: identity.subject,
+    });
+
+    if (!currentUser) {
+      throw new ConvexError("User not found");
+    }
+
+    const membership = await ctx.db
+      .query("conversationMembers")
+      .withIndex("by_memberId_conversationId", (q) =>
+        q
+          .eq("memberId", currentUser._id)
+          .eq("conversationId", args.conversationId),
+      )
+      .unique();
+
+    if (!membership) {
+      throw new ConvexError("You aren't a member of this conversation");
+    }
+
+    const lastMessage = await ctx.db.get(args.messageId);
+
+    await ctx.db.patch(membership._id, {
+      lastSeenMessage: lastMessage ? lastMessage._id : undefined,
+    });
+  },
+});
