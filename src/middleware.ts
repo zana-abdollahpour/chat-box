db12dfe7e6dacfc512818e7232e824279d161672
zana-abdollahpour <zana.ap4@gmail.com>
2024-12-27 01:48:18 +0330

    implement backend support for uploadthing

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,10 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(["/api/uploadthing"]);
+
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) await auth.protect();
+});
 
 export const config = {
   matcher: [
