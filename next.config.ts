498eca7cfe46aec6fb929a6d3019d1262fb9b035
zana-abdollahpour <zana.ap4@gmail.com>
2024-12-27 13:20:49 +0330

    remove unnecessary swcMinify in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   compiler: { removeConsole: process.env.NODE_ENV !== "development" },
   reactStrictMode: true,
-  swcMinify: true,
   images: { domains: ["img.clerk.com", "utfs.io"] },
   async redirects() {
     return [{ source: "/", destination: "/conversations", permanent: true }];
